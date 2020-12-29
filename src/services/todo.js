637d842c640ed5db8d06f6aac8602f664fee2433
Gaspar Neto <gasparneto95@gmail.com>
2020-12-29 17:44:56 -0300

    Create service for integration with the API

---
+++ src/services/todo.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+
+
+const api = axios.create({
+  baseURL: "http://localhost:8080/",
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
+
+
+async function listTodoItems() {
+  const response = await api.get('todo/');
+  return response.data;
+}
+
+async function createTodoItem(todoItem) {
+  console.log(todoItem);
+  const response = await api.post('todo/', todoItem);
+  return response.data;
+}
+
+async function updateTodoItem(id, todoItem) {
+  todoItem.id = id;
+  const response = await api.put('todo/', todoItem);
+  console.log(response.data);
+  return response.data;
+}
+
+async function deleteTodoItem(id) {
+  console.log(id);
+  const response = await api.delete(`todo/${id}`)
+  return response.data
+}
+
+
+export {
+  listTodoItems,
+  createTodoItem,
+  updateTodoItem,
+  deleteTodoItem
+};
